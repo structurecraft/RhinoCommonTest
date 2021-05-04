32c66b930dd664bbad65e2d64596ca5ab33144cc
Mirco Bianchini <sonomirco@gmail.com>
2021-05-05 08:36:13 +1000

    Developed Devide curve method.

--- Lib/CurveSplit.cs
+++ Lib/CurveSplit.cs
@@ -3,16 +3,89 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using Rhino.Geometry;
+using Rhino.Geometry.Intersect;
 
 namespace SplitCurves.Lib
 {
 	public static class Curves
 	{
+        /// <summary>
+        /// Divides curve by a collection of planes.
+        /// </summary>
+        /// <param name="boundary">Closed curve.</param>
+        /// <param name="planes">Collection of planes, must be parallel.</param>
+        /// <param name="tolerance">Tolerance set per default to 1e-3.</param>
+        /// <returns>A set of closed curve.</returns>
+        public static List<Curve> DivideCurve(Curve boundary, List<Plane> planes, double tolerance = 1e-3)
+        {
+            if (planes == null || planes.Count == 0)
+            {
+                throw new Exception("Input planes is null or empty!");
+            }
 
-		public static List<Curve> DivideCurve(Curve boundary, List<Plane> planes)
-		{
-			return null;
-		}
+            for (int i = 1; i < planes.Count; i++)
+            {
+                if (planes[0].Normal.IsParallelTo(planes[i].Normal) == 0)
+                {
+                    throw new Exception("Planes should be parallel!");
+                }
+            }
 
-	}
+            if (!boundary.IsClosed)
+            {
+                throw new Exception("Boundary should be closed!");
+            }
+
+            Curve splitBoundary = boundary;
+            List<Curve> crvResult = new List<Curve>();
+
+            for (int i = 0; i < planes.Count; i++)
+            {
+                CurveIntersections intersection = Intersection.CurvePlane(splitBoundary, planes[i], tolerance);
+                if (intersection == null || intersection.Count == 0) continue;
+
+                Curve[] crvs = splitBoundary.Split(new double[] { intersection[0].ParameterA, intersection[1].ParameterA });
+                Curve tempCurve = new Line(intersection[0].PointA, intersection[1].PointA).ToNurbsCurve();
+
+                // Close the two halves using the curve generated between the intersection points.
+                // Retrieve the signed distance to understand which side of the plane is the the new boundary.
+                Curve c0 = Curve.JoinCurves(new Curve[] { tempCurve, crvs[0] }, tolerance).First();
+                double d0 = SignedDistance(planes[i], c0);
+
+                Curve c1 = Curve.JoinCurves(new Curve[] { tempCurve, crvs[1] }, tolerance).First();
+                double d1 = SignedDistance(planes[i], c1);
+
+                // Collect one boundary and update the other for the next split.
+                if (i == planes.Count - 1)
+                {
+                    crvResult.Add(c0);
+                    crvResult.Add(c1);
+                }
+                else
+                {
+                    if (d0 < d1)
+                    {
+                        splitBoundary = c0;
+                        crvResult.Add(c1);
+                    }
+                    else
+                    {
+                        splitBoundary = c1;
+                        crvResult.Add(c0);
+                    }
+                }
+            }
+
+            return crvResult;
+        }
+
+
+
+        private static double SignedDistance(Plane plane, Curve crv)
+        {
+            var m1 = AreaMassProperties.Compute(crv).Centroid;
+            Vector3d m1ToOrigin = plane.Origin - m1;
+            return m1ToOrigin * plane.Normal;
+        }
+    }
 }
