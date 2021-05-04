9dc7269f1525165846186993be2284e5d2b46350
Mirco Bianchini <sonomirco@gmail.com>
2021-05-05 08:43:11 +1000

    Sets the command.

--- Plugin/SplitCurveCommand.cs
+++ Plugin/SplitCurveCommand.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using Rhino;
 using Rhino.Commands;
+using Rhino.Display;
 using Rhino.DocObjects;
 using Rhino.Geometry;
 using Rhino.Input;
 using Rhino.Input.Custom;
+using SplitCurves.Lib;
 
 namespace SplitCurves.Plugin
 {
@@ -26,7 +28,70 @@ namespace SplitCurves.Plugin
 
 		protected override Result RunCommand(RhinoDoc doc, RunMode mode)
 		{
-			return Result.Success;
-		}
-	}
+            var selectedBoundary = RhinoGet.GetOneObject(
+                "Select one closed curve", false, ObjectType.Curve, out ObjRef boundary);
+            if (selectedBoundary != Result.Success)
+                return selectedBoundary;
+
+            var getPoint = new GetPoint();
+            List<Point3d> pts = new List<Point3d>();
+            while (true)
+            {
+                getPoint.SetCommandPrompt("click location to create point. Click esc to exit.");
+                getPoint.AcceptNothing(true);
+                getPoint.Get();
+                if (getPoint.CommandResult() != Result.Success)
+                    break;
+                pts.Add(getPoint.Point());
+            }
+
+            var getPlaneNormal = new VectorGetter("End point of vector");
+            getPlaneNormal.Get();
+            if (getPlaneNormal.CommandResult() != Result.Success)
+                return getPlaneNormal.CommandResult();
+
+            Vector3d normal = getPlaneNormal.Point() - getPlaneNormal.m_base;
+
+            List<Plane> planes = new List<Plane>();
+            foreach (Point3d pt in pts)
+            {
+                planes.Add(new Plane(pt, normal));
+            }
+
+            var crv = Curves.DivideCurve(boundary.Curve(), planes, doc.ModelAbsoluteTolerance);
+
+            foreach (var curve in crv)
+            {
+                doc.Objects.AddCurve(curve);
+            }
+            //doc.Views.Redraw();
+            return Result.Success;
+        }
+
+        internal class VectorGetter : GetPoint
+        {
+            internal Point3d m_base = Point3d.Origin;
+            internal Line m_line;
+
+            public VectorGetter(string prompt)
+            {
+                this.SetCommandPrompt(prompt);
+                this.AcceptNothing(true);
+                this.SetBasePoint(this.m_base, true);
+                this.MouseMove += new EventHandler<GetPointMouseEventArgs>(this.LocalMouseMove);
+                this.DynamicDraw += new EventHandler<GetPointDrawEventArgs>(this.LocalDynamicDraw);
+            }
+
+            private void LocalMouseMove(object sender, GetPointMouseEventArgs e) => this.m_line = new Line(this.m_base, e.Point);
+
+            private void LocalDynamicDraw(object sender, GetPointDrawEventArgs e)
+            {
+                PointStyle previewPointStyle = PointStyle.Circle;
+                e.Display.DrawPoint(this.m_base, previewPointStyle, 10, System.Drawing.Color.DarkBlue);
+                if (!this.m_line.IsValid)
+                    return;
+                e.Display.DrawArrow(this.m_line, System.Drawing.Color.DarkBlue);
+            }
+        }
+    }
 }
